547a66636722712421b9da7a1cf162428ee8cb82
ryotogashi <ryo.togashi.ca@gmail.com>
2019-07-31 11:20:16 -0700

    add user input in console.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import readline from "readline-sync";
 import Robot from "./models/robot";
 
 function main() {
-  const robot = new Robot("roboko");
+  const robot = new Robot("Roboko");
 
   robot.greeting();
+  robot.clientName = readline.question();
+  robot.farewell();
 }
 
 main();
